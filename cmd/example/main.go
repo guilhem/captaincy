11837cc107eefb96656a64a9f8a274f0c6bd0fe6
Guilhem Lettron <glettron@vente-privee.com>
2017-11-24 01:16:49 +0100

    Working etcd operator
    \o/

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/golang/glog"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/clientcmd"
+
+	appsv1beta1 "k8s.io/api/apps/v1beta1"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 
 	captaincyclientset "github.com/guilhem/captaincy/pkg/client/clientset/versioned"
 
@@ -43,22 +50,125 @@ func main() {
 		glog.Fatalf("Error building etcd clientset: %v", err)
 	}
 
-	fmt.Printf("cluster %v\n", list)
+	k8sClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building etcd clientset: %v", err)
+	}
+
+	apiExtClient, err := apiextensionsclientset.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building etcd clientset: %v", err)
+	}
 
 	for _, cluster := range list.Items {
-		fmt.Printf("cluster %s\n", cluster)
 
-		etcdCl := etcdcluster.EtcdCluster{
-			Spec: etcdcluster.ClusterSpec{
-				Size: 3,
-			},
+		if err := createEtcdOperator(k8sClient, cluster.Namespace); err != nil {
+			glog.Errorf("Error spawning ETCD operator: %v", err)
 		}
-		fmt.Println(cluster.Namespace)
-		res, err := etcdClient.EtcdV1beta2().EtcdClusters(cluster.Namespace).Create(&etcdCl)
-		if err != nil {
+
+		if _, err := apiExtClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get("etcdclusters.etcd.database.coreos.com", metav1.GetOptions{}); err != nil {
+
+			wi, err := apiExtClient.ApiextensionsV1beta1().CustomResourceDefinitions().Watch(metav1.ListOptions{
+				TimeoutSeconds: int64Ptr(30),
+				FieldSelector:  fields.OneTermEqualSelector("metadata.name", "etcdclusters.etcd.database.coreos.com").String(),
+			})
+			if err != nil {
+				glog.Errorf("Error spawning ETCD cluster: %v", err)
+			}
+			defer wi.Stop()
+
+			select {
+			case watchEvent := <-wi.ResultChan():
+				if watch.Added == watchEvent.Type {
+					glog.Info("etcd operator register")
+					wi.Stop()
+				} else {
+					glog.Errorf("expected add, but got %#v", watchEvent)
+				}
+			}
+		} else {
+			glog.Info("etcdclusters.etcd.database.coreos.com exist")
+		}
+
+		if err := createEtcdCluster(etcdClient, cluster.Name, cluster.Namespace); err != nil {
 			glog.Errorf("Error spawning ETCD cluster: %v", err)
 		}
-		glog.Infof("Etcd created %v", res)
+		glog.Infof("Etcd created")
 	}
 
 }
+
+func createEtcdOperator(client *kubernetes.Clientset, ns string) error {
+	if _, err := client.AppsV1beta1().Deployments(ns).Get("etcd-operator", metav1.GetOptions{}); err == nil {
+		return nil
+	}
+
+	deployment := &appsv1beta1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "etcd-operator",
+		},
+		Spec: appsv1beta1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"name": "etcd-operator",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:    "etcd-operator",
+							Image:   "quay.io/coreos/etcd-operator:v0.7.0",
+							Command: []string{"etcd-operator"},
+							Env: []apiv1.EnvVar{
+								{
+									Name: "MY_POD_NAMESPACE",
+									ValueFrom: &apiv1.EnvVarSource{
+										FieldRef: &apiv1.ObjectFieldSelector{
+											FieldPath: "metadata.namespace",
+										},
+									},
+								},
+								{
+									Name: "MY_POD_NAME",
+									ValueFrom: &apiv1.EnvVarSource{
+										FieldRef: &apiv1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err := client.AppsV1beta1().Deployments(ns).Create(deployment)
+	return err
+}
+
+func createEtcdCluster(client *etcdclientset.Clientset, name string, ns string) error {
+	if _, err := client.EtcdV1beta2().EtcdClusters(ns).Get(name, metav1.GetOptions{}); err == nil {
+		return nil
+	}
+
+	etcdCl := etcdcluster.EtcdCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"captaincy": "kinky",
+			},
+		},
+		Spec: etcdcluster.ClusterSpec{
+			Size: 3,
+		},
+	}
+
+	_, err := client.EtcdV1beta2().EtcdClusters(ns).Create(&etcdCl)
+	return err
+}
+
+func int32Ptr(i int32) *int32 { return &i }
+func int64Ptr(i int64) *int64 { return &i }
--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -51,8 +51,8 @@ var (
 	// semantic version is a git hash, but the version itself is no
 	// longer the direct output of "git describe", but a slight
 	// translation to be semver compliant.
-	gitVersion   string = "v1.8.0-beta.1+7b06b96"
-	gitCommit    string = "7b06b96a073b61ac95777ec2dbe0e4a3bc08c8b2"    // sha1 from git, output of $(git rev-parse HEAD)
+	gitVersion   string = "v1.8.0-beta.1+$Format:%h$"
+	gitCommit    string = "$Format:%H$"    // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState string = "not a git tree" // state of git tree, either "clean" or "dirty"
 
 	buildDate string = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
